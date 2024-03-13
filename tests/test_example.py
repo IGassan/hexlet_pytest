2b5e5bec26ce595132b69c2daddbbfef71d2935b
IGassan <igasan96@gmail.com>
2024-03-13 15:54:34 +0300

    add stack

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,8 +2,26 @@ from hexlet_pytest.example import reverse
 
 
 def test_reverse():
-    assert reverse('Hexlet') != 'telxeH'
+    assert reverse('Hexlet') == 'telxeH'
 
 
 def test_reverse_for_empty_string():
     assert reverse('') == ''
+
+
+def test_stack():
+    stack = []
+    stack.append('one')
+    stack.append('two')
+
+    assert stack.pop() == 'two'
+    assert stack.pop() == 'one'
+
+
+def test_emptiness():
+    stack = []
+    assert not stack
+    stack.append('one')
+    assert bool(stack)
+    stack.pop()
+    assert not stack
